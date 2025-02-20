8c5b426f5129d0361362df58227b7677e920be81
Harindi Navodya <141401570+navodyayashodani@users.noreply.github.com>
2025-02-20 21:02:41 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -33,5 +33,5 @@ app.use("/api/payments", paymentsRouter);
 app.use(globalErrorHandlingMiddleware as any);
 
 connectDB();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
